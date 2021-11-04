6219a8584e92a961d7573134841570207517baf6
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-11-04 18:48:32 +0100

    aded routes

---
+++ server/routes/subjectsRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/things", (req, res) => {});
+
+router.get("/things", (req, res) => {});
+
+router.get("/things/:idThing", (req, res) => {});
+
+router.delete("/things/:idThing", (req, res) => {});
+
+router.post("/things/:idThing", (req, res) => {});
+
+router.put("/things/:idThing", (req, res) => {});
