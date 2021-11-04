e4d4e2d94d35527ca0120381a9d69ff1dd241daa
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-11-04 19:33:17 +0100

    added conection

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const chalk = require("chalk");
+const debug = require("debug")("pets:server");
+const express = require("express");
+const morgan = require("morgan");
+const subjectRoutes = require("./routes/subjectsRoutes");
+
+const app = express();
+
+const initializeServer = (port) => {
+  const server = app.listen(port, () => {
+    debug(chalk.yellow(`Escuchando en el puerto ${port}`));
+  });
+
+  server.on("error", (error) => {
+    debug(chalk.red("Ha habido un error al iniciar el servidor."));
+    if (error.code === "EADDRINUSE") {
+      debug(chalk.red(`El puerto ${port} está en uso.`));
+    }
+  });
+};
+
+app.use(morgan("dev"));
+
+module.exports = initializeServer;
--- server/routes/subjectsRoutes.js
+++ server/routes/subjectsRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/things", (req, res) => {});
+// router.get("/things", (req, res) => {});
 
-router.get("/things", (req, res) => {});
+// router.get("/things", (req, res) => {});
 
-router.get("/things/:idThing", (req, res) => {});
+// router.get("/things/:idThing", (req, res) => {});
 
-router.delete("/things/:idThing", (req, res) => {});
+// router.delete("/things/:idThing", (req, res) => {});
 
-router.post("/things/:idThing", (req, res) => {});
+// router.post("/things/:idThing", (req, res) => {});
 
-router.put("/things/:idThing", (req, res) => {});
+// router.put("/things/:idThing", (req, res) => {});
+
+module.exports = router;
